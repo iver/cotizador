1f540b1a8333da6b87e5474659db1b7445c9af5d
Ivan Jaimes <ivan.jaimes@iver.mx>
2014-07-25 08:22:59 -0500

    Add new fields and datepicker control

--- public/js/negocio.js
+++ public/js/negocio.js
@@ -1,12 +1,51 @@
 "use strict";
 (function(sifsa, $, undefined){
   var plagas = $('#selectPlagas');
-  sifsa.valida = function (){
-    alert('Total de plagas '+plagas.select2('val'));
+  sifsa.estimate = function (){
+    sifsa.showSuccess("Wey!", "Te falta revisar");
   };
+
+  sifsa.message = {
+    base: function(action, message, msgType){
+       return [
+        '<div class="alert alert-', msgType, '">',
+           '<a href="#" class="close" data-dismiss="alert">&times;</a>',
+           '<strong>', action, '</strong> ', message,
+        '</div>'
+       ].join('');
+     },
+     sent: function(action, message) {
+       return this.base(action, message, "success");
+     },
+     error: function(action, message) {
+       return this.base(action, message, "danger");
+     }
+  };
+
+  var output = $('#output');
+
+  sifsa.showError = function(action, message) {
+     var html = sifsa.message.error(action, message);
+     output.append(html);
+  };
+  sifsa.showSuccess = function(action, message) {
+     var html = sifsa.message.sent(action, message);
+     output.append(html);
+  };
+
+
 }(window.sifsa = window.sifsa || {}, jQuery));
 
 $(function() {
+
+  $('#dateControl .input-group.date').datepicker({
+      todayBtn: "linked",
+      language: "es",
+      daysOfWeekDisabled: "0",
+      autoclose: true,
+      todayHighlight: true
+  });
+
   $("#selectExtension").select2({
     width: 165
   });
@@ -24,5 +63,5 @@ $(function() {
     width: 165
   });
 
-  $('#btnValida').on("click", sifsa.valida);
+  $('#btnEstimate').on("click", sifsa.estimate);
 });
